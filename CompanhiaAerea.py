504db1bb33de3918b1adecf6dbb5b36388520d7a
Anderson Campos <anderson.camposs87@outlook.com>
2024-12-09 11:37:56 -0300

    Service da entidade reserva

--- CompanhiaAerea.py
+++ CompanhiaAerea.py
@@ -164,32 +164,33 @@ class CompanhiaAerea:
                 elif(opcao == "1"):
                     print("============================")
                     cpfCliente = input("INFORME O CPF DO CLIENTE: ")
-                    cliente = ClienteDAO().getByCpf(cpfCliente)
-                    if (cliente == None):
-                        MessageManager.customMessage("CLIENTE NÃO ENCONTRADO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
-                        os.system("cls")
-                        continue
-                    
                     idVoo = input("INFORME O ID DO VOO: ")
-                    voo = VooDAO().getById(idVoo)
-                    if(voo == None):
-                        MessageManager.customMessage("VOO NÃO ENCONTRADO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
-                        os.system("cls")
-                        continue
-
-                    dataAtual = date.today().strftime("%d/%m/%Y")
                     try:
-                        valor = float(input("INFORME O VALOR DA RESERVA: "))
-                        reserva = Reserva(id = None, cliente = cliente.id, voo = voo.id, data=dataAtual, valor=valor)
-                        ReservaDAO().insert(reserva=reserva)
-                        MessageManager.customMessage("RESERVA CADASTRADA COM SUCESSO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.success)
-                    except ValueError:
-                        MessageManager.customMessage("VALOR DA RESERVA INVÁLIDO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
+                        cliente = ClienteDAO().getByCpf(cpfCliente)
+                    
+                        voo = VooDAO().getById(idVoo)
+
+                        dataAtual = date.today().strftime("%d/%m/%Y")
+                    
+                        try:
+                            valor = float(input("INFORME O VALOR DA RESERVA: "))
+                            
+                            reserva = Reserva(id = None, cliente = cliente.id, voo = voo.id, data=dataAtual, valor=valor)
+                            ReservaDAO().insert(reserva=reserva)
+                            MessageManager.customMessage("RESERVA CADASTRADA COM SUCESSO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.success)
+                        
+                        except ValueError:
+                            MessageManager.customMessage("VALOR DA RESERVA INVÁLIDO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
+                        
+                    except RegisterNotFoundException as e:
+                        MessageManager.customMessage(f"{str(e)}, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
+                    
+                    
 
                 elif(opcao == "2"):
                     id = input("INFORME O ID DA RESERVA: ")
-                    reserva = ReservaDAO().getById(id)
-                    if(reserva != None):
+                    try:
+                        reserva = ReservaDAO().getById(id)
                         cliente = ClienteDAO().getById(reserva.idCliente)
                         voo = VooDAO().getById(reserva.idVoo)
                         table = PrettyTable()
@@ -197,26 +198,26 @@ class CompanhiaAerea:
                         table.add_row([reserva.id, cliente.nome, cliente.cpf, cliente.email, voo.origem, voo.destino, voo.data, reserva.data, reserva.valor])
                         print(table)
                         MessageManager.customMessage("PRESSIONE ENTER PARA CONTINUAR", MessageManager.info)
-                    else:
-                        MessageManager.customMessage("RESERVA NÃO ENCONTRADA, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
+                    except RegisterNotFoundException as e:
+                        MessageManager.customMessage(f"{str(e)}, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
 
                 elif(opcao == "3"):
                     print("============================")
                     idVoo = input("INFORME O ID DO VOO: ")
-                    voo = VooDAO().getById(int(idVoo))
-                    if(voo == None):
-                        MessageManager.customMessage("VOO NÃO ENCONTRADO, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
-                        os.system("cls")
-                        continue
+                    try:
+                        voo = VooDAO().getById(int(idVoo))
                     
-                    listaReservas = ReservaDAO().listByVooId(idVoo)
-                    table = PrettyTable()
-                    table.field_names = ["ID DA RESERVA", "NOME DO CLIENTE", "CPF DO CLIENTE", "E-MAIL DO CLIENTE", "ORIGEM DO VOO", "DESTINO DO VOO", "DATA DO VOO", "DATA DA RESERVA", "VALOR DA RESERVA"]
-                    for reserva in listaReservas:
-                        table.add_row([reserva.id, reserva.cliente.nome, reserva.cliente.cpf, reserva.cliente.email, reserva.voo.origem, reserva.voo.destino, reserva.voo.data, reserva.data, reserva.valor], divider=True)
+                        listaReservas = ReservaDAO().listByVooId(idVoo)
+                        table = PrettyTable()
+                        table.field_names = ["ID DA RESERVA", "NOME DO CLIENTE", "CPF DO CLIENTE", "E-MAIL DO CLIENTE", "ORIGEM DO VOO", "DESTINO DO VOO", "DATA DO VOO", "DATA DA RESERVA", "VALOR DA RESERVA"]
+                        for reserva in listaReservas:
+                            table.add_row([reserva.id, reserva.cliente.nome, reserva.cliente.cpf, reserva.cliente.email, reserva.voo.origem, reserva.voo.destino, reserva.voo.data, reserva.data, reserva.valor], divider=True)
 
-                    print(table)
-                    MessageManager.customMessage("PRESSIONE ENTER PARA CONTINUAR", MessageManager.info)
+                        print(table)
+                        MessageManager.customMessage("PRESSIONE ENTER PARA CONTINUAR", MessageManager.info)
+                    except RegisterNotFoundException as e:
+                        MessageManager.customMessage(f"{str(e)}, PRESSIONE ENTER PARA CONTINUAR", MessageManager.danger)
+                
                 else:
                     MessageManager.invalidOption()
 
